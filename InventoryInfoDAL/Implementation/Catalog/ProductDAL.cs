8f8f1986f2dde7d8bcd6b95dae3724b725c22f55
Jua Navarro <juan.navarro.ortz@gmail.com>
2024-03-21 06:23:02 -0600

    Saving changes in interface and implementation

--- InventoryInfoDAL/Implementation/Catalog/ProductDAL.cs
+++ InventoryInfoDAL/Implementation/Catalog/ProductDAL.cs
@@ -7,10 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using InventoryInfoDAL.Models;
+using InventoryInfoDAL.Interfaces.Catalog;
 
 namespace InventoryInfoDAL.Implementation
 {
-    public class ProductDAL
+    public class ProductDAL<T> : IProductDAL<T> where T : class
     {
         //Instance to DBContext
         private readonly InventoryInformationEntities dbContext;
@@ -22,13 +23,48 @@ namespace InventoryInfoDAL.Implementation
             ErrorLog = errorLog;
         }
 
-        public async Task DeleteAsync(int Id)
+
+        public async Task DeleteAsync(int Id, bool saveChanges = true)
         {
-            var entity = await dbContext.Products.FindAsync(Id);
+            var product = await dbContext.Products.FindAsync(Id);
+            await DeleteAsync(product.Id);
+            if (saveChanges)
+               await dbContext.SaveChangesAsync();
         }
 
+        public async Task DeleteAsync(T entity, bool saveChanges = true)
+        {
+           // dbContext.Products.Remove(entity);
+        }
 
+        public Task DeleteRangeAsync(IEnumerable<T> entities, bool saveChanges = true)
+        {
+            throw new NotImplementedException();
+        }
 
+        public T Find(params object[] keyValues)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Task<T> FindAsync(params object[] keyValues)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InsertAsync(T entity, bool saveChanges = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InsertRangeAsync(IEnumerable<T> entity, bool saveChanges = true)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- InventoryInfoDAL/Interfaces/Catalog/IProductDAL.cs
+++ InventoryInfoDAL/Interfaces/Catalog/IProductDAL.cs
@@ -8,7 +8,28 @@ namespace InventoryInfoDAL.Interfaces.Catalog
 {
     public interface IProductDAL<T> where T : class
     {
+        //Get all items of an entity by asynchronous method 
         Task<IList<T>> GetAllAsync();
 
+        //Find one item of an entity synchronous method 
+        T Find(params object[] keyValues);
+
+        //Find one item of an entity asynchronous method 
+        Task<T> FindAsync(params object[] keyValues);
+
+        //Insert item into an entity by asynchronous method
+        Task InsertAsync(T entity, bool saveChanges = true);
+
+        //Insert multiple items into an entity by asynchronous 
+        Task InsertRangeAsync(IEnumerable<T> entity, bool saveChanges = true);
+
+        //Remove one item from an entity by asynchronous method 
+        Task DeleteAsync(int Id, bool saveChanges = true);
+
+        //Remove one item from an entity by asynchronous method 
+        Task DeleteAsync(T entity, bool saveChanges = true);
+
+        //Remove multiple items from an entity by asychronous method 
+        Task DeleteRangeAsync(IEnumerable<T> entities, bool saveChanges = true);
     }
 }
